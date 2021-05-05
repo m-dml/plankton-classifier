Fix random split and implement loss weighting

src/models/LightningBaseModel.py
@@ -13,15 +13,34 @@ import pytorch_lightning.metrics as pl_metrics
 
 class LightningModel(pl.LightningModule):
 
-    def __init__(self, class_labels, *args, **kwargs):
+    def __init__(self, class_labels, all_labels, *args, **kwargs):
 
         super().__init__()
         self.class_labels = class_labels
+        self.all_labels = all_labels
+        self.label_weight_tensor = self.get_label_weights()
         self.model = self.define_model()
         self.learning_rate = kwargs["learning_rate"]
-        self.loss_func = nn.NLLLoss()
+        self.loss_func = nn.NLLLoss(weight=self.label_weight_tensor)
         self.accuracy_func = pl_metrics.Accuracy()
         self.save_hyperparameters()
+
+    def get_label_weights(self):
+        label_weights_dict = dict()
+
+        for label in self.all_labels:
+            if label in label_weights_dict.keys():
+                label_weights_dict[label] += 1
+            else:
+                label_weights_dict[label] = 1
+
+        weights = []
+        for label in self.class_labels:
+            weights.append(1 / label_weights_dict[label])
+
+        weight_tensor = torch.tensor(weights, dtype=torch.float32, device=self.device)
+
+        return weight_tensor
 
     def define_model(self):
         return resnet18(pretrained=False, num_classes=len(self.class_labels))
@@ -37,7 +56,7 @@ class LightningModel(pl.LightningModule):
     def training_step(self, batch, batch_idx, *args, **kwargs):
         images, labels, label_names = batch
 
-        loss, acc = self._do_step(images, labels, label_names, step="Validation", log_images=False)
+        loss, acc = self._do_step(images, labels, label_names, step="Training", log_images=False)
         return loss
 
     def validation_step(self, batch, batch_idx, *args, **kwargs):
@@ -53,12 +72,13 @@ class LightningModel(pl.LightningModule):
     def test_step(self, batch, batch_idx, *args, **kwargs):
         images, labels, label_names = batch
 
-        loss, acc = self._do_step(images, labels, label_names, step="Validation", log_images=False)
+        loss, acc = self._do_step(images, labels, label_names, step="Test", log_images=False)
         return loss
 
     def _do_step(self, images, labels, label_names, step, log_images=False):
 
         predictions = self(images)
+
         loss = self.loss_func(predictions, labels.view(-1).long())
         accuracy = self.accuracy_func(F.softmax(predictions, dim=1).detach().cpu(), labels.to(torch.int).detach().cpu())
 

src/utils/DataLoader.py
@@ -1,14 +1,15 @@
 import glob
 import logging
 import os
+import random
 
 import pytorch_lightning as pl
 import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 from src.utils import CONFIG
-from tqdm import tqdm
 
 
 class PlanktonDataSet(Dataset):
@@ -52,6 +53,7 @@ class PlanktonDataLoader(pl.LightningDataModule):
         self.input_channels = None
         self.output_channels = None
         self.unique_labels = []
+        self.all_labels = []
         self.integer_class_labels = dict()
 
         self.batch_size = CONFIG.batch_size
@@ -115,6 +117,7 @@ class PlanktonDataLoader(pl.LightningDataModule):
                     for file in raw_file_paths:
                         label = os.path.split(folder)[-1]
                         files.append((self.load_image(file, self.preload_dataset),label))
+                        self.all_labels.append(label)
                         if label not in self.unique_labels:
                             self.unique_labels.append(label)
 
@@ -124,6 +127,7 @@ class PlanktonDataLoader(pl.LightningDataModule):
                         for file in raw_file_paths:
                             label = os.path.split(folder)[-1]
                             files.append((self.load_image(file, self.preload_dataset), label))
+                            self.all_labels.append(label)
                             if label not in self.unique_labels:
                                 self.unique_labels.append(label)
 
@@ -133,8 +137,12 @@ class PlanktonDataLoader(pl.LightningDataModule):
                 for file in raw_file_paths:
                     label = os.path.split(folder)[-1]
                     files.append((self.load_image(file, self.preload_dataset), label))
+                    self.all_labels.append(label)
                     if label not in self.unique_labels:
                         self.unique_labels.append(label)
+
+        random.seed(CONFIG.random_seed)
+        random.shuffle(files)
         return files
 
     def train_dataloader(self):