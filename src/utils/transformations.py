3a05c84a68019e58c809a27504bec33476c71424
Kubi <KubiDemir@icloud.com>
2021-05-05 15:23:14 +0200

    Add random_rotations to transformations and config

--- src/utils/transformations.py
+++ src/utils/transformations.py
@@ -19,7 +19,8 @@ def transform_function(transform_type: str = "default", final_image_size: int):
         transform = transforms.RandomApply(
             torch.nn.ModuleList([transforms.RandomCrop(), transforms.RandomRotation(degrees=[-10., 10.])]), p=0.3)
         transform = torch.jit.script(transform)
-
+    elif transform_type == "random_rotations"
+        transform = transforms.RandomRotation(degrees=[-10., 10.])
     return transform
 
 
