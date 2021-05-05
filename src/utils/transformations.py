cb754cd4f5a19e27d51cab9ea0f473d43312751d
Kubi <KubiDemir@icloud.com>
2021-05-05 16:40:58 +0200

    Change span of random rotations to -180 deg to 180 deg and change config file to default_config in train_on_strand

--- src/utils/transformations.py
+++ src/utils/transformations.py
@@ -13,12 +13,12 @@ def transform_function(transform_type: str = "default", final_image_size=100):
              transforms.ToTensor()])
     elif transform_type == "random_rotations":
         transform = transforms.Compose(
-            [transforms.RandomRotation(degrees=[-180., 180°.]), transforms.Pad(final_image_size),
+            [transforms.RandomRotation(degrees=[-180., 180.]), transforms.Pad(final_image_size),
              transforms.CenterCrop([final_image_size, final_image_size]), transforms.ToTensor()])
     elif transform_type == "random_transforms":
         # Apply transforms out of a list of transforms randomly for a given probability
         random_transforms = transforms.RandomApply(
-            torch.nn.ModuleList([transforms.RandomCrop(), transforms.RandomRotation(degrees=[-10., 10.])]), p=0.3)
+            torch.nn.ModuleList([transforms.RandomCrop(), transforms.RandomRotation(degrees=[-180., 180.])]), p=0.3)
         random_transforms = torch.jit.script(random_transforms)
         transform = transforms.Compose([random_transforms(), transforms.Pad(final_image_size),
              transforms.CenterCrop([final_image_size, final_image_size]), transforms.ToTensor()])
