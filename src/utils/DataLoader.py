458d5e5a740af46a52db60a7ff51ed8b819a525c
Tobias <tobias-machnitzki@web.de>
2021-05-20 09:31:02 +0200

    Dataloading for the new planktonnet data (#26)
    * Dataloading for the new planktonnet data
    
    Also better structure for loading images from folder names
    in general by outsourcing the actual data loading
    
    * Add some tests for data loading

--- src/utils/DataLoader.py
+++ src/utils/DataLoader.py
@@ -1,6 +1,7 @@
 import glob
 import logging
 import os
+import pathlib
 import random
 
 import pytorch_lightning as pl
@@ -60,7 +61,7 @@ class PlanktonDataLoader(pl.LightningDataModule):
         self.output_channels = None
         self.unique_labels = []
         self.all_labels = []
-        self.integer_class_labels = dict()
+        self.integer_class_label_dict = dict()
 
         self.excluded_labels = CONFIG.excluded_classes
         self.batch_size = CONFIG.batch_size
@@ -71,21 +72,18 @@ class PlanktonDataLoader(pl.LightningDataModule):
         self.shuffle_validation_dataset = CONFIG.shuffle_validation_dataset
         self.shuffle_test_dataset = CONFIG.shuffle_test_dataset
         self.preload_dataset = CONFIG.preload_dataset
-        self.old_data_path = os.path.join(CONFIG.plankton_data_base_path, CONFIG.old_sorted_plankton_data)
         self.new_data_path = os.path.join(CONFIG.plankton_data_base_path, CONFIG.new_sorted_plankton_data)
-
-        self.use_old_data = CONFIG.use_old_data
-        self.use_new_data = CONFIG.use_new_data
-        self.use_subclasses = CONFIG.use_subclasses
+        self.planktonnet_data_path = os.path.join(CONFIG.plankton_data_base_path, CONFIG.planktonnet_data)
+        self.use_planktonnet_data = CONFIG.use_planktonnet_data
+        self.use_klas_data = CONFIG.use_klas_data
         self.preload_dataset = CONFIG.preload_dataset
         self.super_classes = CONFIG.super_classes
         self.oversample_data = CONFIG.oversample_data
-
         self.final_image_size = CONFIG.final_image_size
 
     def setup(self, stage=None):
         training_pairs = self.prepare_data_setup()
-        self.integer_class_labels = self.set_up_integer_class_labels()
+        self.integer_class_label_dict = self.set_up_integer_class_labels()
 
         if len(training_pairs) == 0:
             raise FileNotFoundError(f"Did not find any files")
@@ -107,59 +105,42 @@ class PlanktonDataLoader(pl.LightningDataModule):
 
         if stage == 'fit' or stage is None:
             self.train_data = PlanktonDataSet(train_subset, transform=self.transform,
-                                              integer_labels=self.integer_class_labels)
+                                              integer_labels=self.integer_class_label_dict)
             logging.debug(f"Number of training samples: {len(self.train_data)}")
             self.valid_data = PlanktonDataSet(valid_subset, transform=self.transform,
-                                              integer_labels=self.integer_class_labels)
+                                              integer_labels=self.integer_class_label_dict)
             logging.debug(f"Number of validation samples: {len(self.valid_data)}")
 
         if stage == 'test' or stage is None:
             self.test_data = PlanktonDataSet(test_subset, transform=self.transform,
-                                             integer_labels=self.integer_class_labels)
+                                             integer_labels=self.integer_class_label_dict)
 
     def prepare_data_setup(self):
-        excluded = self.excluded_labels
         files = []
-        if self.use_old_data:
-            for folder in tqdm(glob.glob(os.path.join(self.old_data_path, "*")), desc="Load old data"):
-                if not self.use_subclasses:
-                    raw_file_paths = glob.glob(folder + "*/*/*.tif")
-                    for file in raw_file_paths:
-                        label = os.path.split(folder)[-1]
-                        if label in excluded:
-                            continue
-                        files.append((self.load_image(file, self.preload_dataset),label))
-                        self.all_labels.append(label)
-                        if label not in self.unique_labels:
-                            self.unique_labels.append(label)
+        if self.use_klas_data:
+            for folder in tqdm(glob.glob(os.path.join(self.new_data_path, "*")), desc="Load Klas data"):
+                files += self._add_data_from_folder(folder, file_ext="png")
 
-                else:
-                    for sub_folder in tqdm(glob.glob(os.path.join(folder, "*")), desc="Load old data"):
-                        raw_file_paths = glob.glob(sub_folder + "*/*.tif")
-                        for file in raw_file_paths:
-                            label = os.path.split(folder)[-1]
-                            if label in excluded:
-                                continue
-                            files.append((self.load_image(file, self.preload_dataset), label))
-                            self.all_labels.append(label)
-                            if label not in self.unique_labels:
-                                self.unique_labels.append(label)
-
-        if self.use_new_data:
-            for folder in tqdm(glob.glob(os.path.join(self.new_data_path, "*")), desc="Load new data"):
-                raw_file_paths = glob.glob(folder + "*/*/*.png")
-                for file in raw_file_paths:
-                    label = os.path.split(folder)[-1]
-                    label = self._find_super_class(label)
-                    if label in excluded:
-                        continue
-                    files.append((self.load_image(file, self.preload_dataset), label))
-                    self.all_labels.append(label)
-                    if label not in self.unique_labels:
-                        self.unique_labels.append(label)
+        if self.use_planktonnet_data:
+            for folder in tqdm(glob.glob(os.path.join(self.planktonnet_data_path, "*")), desc="Load planktonNet"):
+                files += self._add_data_from_folder(folder, file_ext="jpg")
 
         random.seed(CONFIG.random_seed)
         random.shuffle(files)
+        return files
+
+    def _add_data_from_folder(self, folder, file_ext="png"):
+        files = []
+        for file in pathlib.Path(folder).rglob(f'*.{file_ext}'):
+            label = os.path.split(folder)[-1]
+            label = self._find_super_class(label)
+            if label in self.excluded_labels:
+                continue
+            files.append((self.load_image(file, self.preload_dataset), label))
+            self.all_labels.append(label)
+            if label not in self.unique_labels:
+                self.unique_labels.append(label)
+
         return files
 
     def _find_super_class(self, label):
--- tests/test_DataLoader.py
+++ tests/test_DataLoader.py
@@ -10,35 +10,34 @@ class TestPlanktonDataLoader(unittest.TestCase):
         config_file = "../tobis_config.yaml"
         CONFIG.update(config_file)
 
-    def test_dataset(self):
-        pass
-
-    def test_dataloader_old_data_group_classes(self):
-        new_config = dict(use_subclasses=False,
-                          use_old_data=True,
-                          use_new_data=False,
+    def test_dataloader_klas_data(self):
+        new_config = dict(use_klas_data=True,
+                          use_planktonnet_data=False,
                           preload_dataset=False
                           )
         CONFIG.update(new_config)
         dl = PlanktonDataLoader()
         dl.setup()
 
-    def test_dataloader_old_data_subclasses(self):
-        new_config = dict(use_subclasses=True,
-                          use_old_data=True,
-                          use_new_data=False,
+        self._dataloader_tests(dl)
+
+    def test_dataloader_on_planktonnet(self):
+        new_config = dict(use_klas_data=False,
+                          use_planktonnet_data=True,
                           preload_dataset=False
                           )
         CONFIG.update(new_config)
         dl = PlanktonDataLoader()
         dl.setup()
 
-    def test_dataloader_new_data(self):
-        new_config = dict(use_subclasses=True,
-                          use_old_data=False,
-                          use_new_data=True,
-                          preload_dataset=False
-                          )
-        CONFIG.update(new_config)
-        dl = PlanktonDataLoader()
-        dl.setup()
+        self._dataloader_tests(dl)
+
+    def _dataloader_tests(self, dl):
+        self.assertIsNotNone(dl.integer_class_label_dict)
+        for integer_label in dl.integer_class_label_dict.values():
+            self.assertIsInstance(integer_label, int)
+
+        self.assertFalse([] == dl.unique_labels)
+        self.assertIsNotNone(dl.train_data)
+        self.assertIsNotNone(dl.valid_data)
+        self.assertIsNotNone(dl.test_data)
