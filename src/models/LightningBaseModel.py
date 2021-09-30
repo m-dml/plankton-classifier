adbcfe8122ceddf441e339f9d9d985cc1fc65ad1
Tobias Machnitzki <tobias.machnitzki@hzg.de>
2021-09-30 10:51:33 +0200

    Change TSNE plotting

--- src/models/LightningBaseModel.py
+++ src/models/LightningBaseModel.py
@@ -242,12 +242,23 @@ class LightningModel(pl.LightningModule):
         class_names = [class_label_dict[label] for label in labels]
         df = pd.DataFrame({"x": tx.flatten(), "y": ty.flatten(), "label": class_names}).sort_values(by="label")
 
-        fig, ax = plt.subplots(figsize=(16, 12))
+        fig, ax1= plt.subplots(figsize=(8, 8))
+        num_points = len(df)
+        sns.jointplot(x="x", y="y", hue="label", data=df, ax=ax1, palette="deep", kind='kde', fill=True,
+                      joint_kws={'alpha': 0.4})
+        ax1.legend(loc=4)
+        plt.tight_layout()
+        self.logger.experiment[0].add_figure("TSNE KDE", fig, self.global_step)
+        plt.close("all")
 
-        sns.scatterplot(x="x", y="y", hue="label", data=df, ax=ax, palette="deep")
-        ax.legend(loc=4)
-        plt.gca().invert_yaxis()
-        self.logger.experiment[0].add_figure("TSNE Plot", fig, self.global_step)
+        del fig
+
+        fig, ax2= plt.subplots(figsize=(8, 8))
+        sns.jointplot(x="x", y="y", hue="label", data=df, ax=ax2, palette="deep", marginal_ticks=True)
+        ax2.legend(loc=4)
+        plt.suptitle(f"TSNE regression | {num_points} points")
+        plt.tight_layout()
+        self.logger.experiment[0].add_figure("TSNE Scatter", fig, self.global_step)
         plt.close("all")
 
     def on_save_checkpoint(self, checkpoint) -> None:
