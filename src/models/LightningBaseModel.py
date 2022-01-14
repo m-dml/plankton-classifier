fix total_step calculation

src/models/LightningBaseModel.py
@@ -105,10 +105,10 @@ class LightningModel(pl.LightningModule):
                     else self.batch_size
                 )
                 self.console_logger.info("global batch size is {}".format(global_batch_size))
-                train_iters_per_epoch = len(self.trainer.datamodule.train_data) // global_batch_size
+                train_iters_per_epoch = len(self.trainer.datamodule.train_data) / global_batch_size
                 self.console_logger.info(f"train iterations per epoch are {train_iters_per_epoch}")
-                warmup_steps = train_iters_per_epoch * 10
-                total_steps = train_iters_per_epoch * self.trainer.max_epochs
+                warmup_steps = int(train_iters_per_epoch * 10)
+                total_steps = int(train_iters_per_epoch * self.trainer.max_epochs)
                 scheduler = {
                     "scheduler": torch.optim.lr_scheduler.LambdaLR(
                         optimizer, linear_warmup_decay(warmup_steps, total_steps, cosine=True)
@@ -118,9 +118,9 @@ class LightningModel(pl.LightningModule):
                     "name": "Lars-LR",
                 }
             elif self.cfg_scheduler == "cosine":
-                train_iters_per_epoch = len(self.trainer.datamodule.train_data) // self.batch_size
+                train_iters_per_epoch = len(self.trainer.datamodule.train_data) / self.batch_size
                 self.console_logger.info(f"train iterations per epoch are {train_iters_per_epoch}")
-                total_steps = train_iters_per_epoch * self.trainer.max_epochs
+                total_steps = int(train_iters_per_epoch * self.trainer.max_epochs)
                 scheduler = {
                     "scheduler": torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, total_steps, eta_min=0.0),
                     "interval": "step",