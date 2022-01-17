fix num train batches calculation
closes #36
closes #37

src/datamodule/DataLoader.py
@@ -215,9 +215,6 @@ class PlanktonDataLoader(pl.LightningDataModule):
 
         _, self.training_class_counts = np.unique(self.train_labels, return_counts=True)
 
-        if self.subsample_supervised < 100:
-            train_subset, self.train_labels = self._resample_data(train_subset, self.train_labels)
-
         self.console_logger.info(f"There are {len(train_subset)} training files")
         self.console_logger.info(f"There are {len(valid_subset)} validation files")
         if stage == "fit" or stage is None:
@@ -348,29 +345,9 @@ class PlanktonDataLoader(pl.LightningDataModule):
                     break
             return label
 
-    def _resample_data(self, train_data, train_labels):
-
-        subsample_proportion = self.subsample_supervised / 100
-        samples_per_class = np.floor(len(train_labels) * subsample_proportion / len(np.unique(train_labels)))
-        self.console_logger.info(f"Only use {samples_per_class} samples per class.")
-
-        new_train_data = []
-        new_train_labels = []
-        train_dict = {}
-
-        for datum, label in zip(train_data, train_labels):
-            if label not in train_dict.keys():
-                train_dict[label] = []
-            if len(train_dict[label]) <= samples_per_class:
-                train_dict[label].append(datum)
-                new_train_data.append(datum)
-                new_train_labels.append(label)
-
-        return new_train_data, np.array(new_train_labels)
-
     def train_dataloader(self):
         if self.oversample_data:
-            sampler = BalanceClassSampler(self.train_labels, mode="upsampling")
+            sampler = BalanceClassSampler(self.train_labels, mode=self.subsample_supervised)
             if self.is_ddp:
                 self.console_logger.info("Wrapping the sampler to be ddp compatible")
                 sampler = self.ddp_wrap_sampler(sampler)
@@ -426,3 +403,7 @@ class PlanktonDataLoader(pl.LightningDataModule):
 
     def ddp_wrap_sampler(self, sampler):
         return DistributedSamplerWrapper(sampler)
+
+    @property
+    def len_train_data(self):
+        return len(self.train_dataloader())

src/models/LightningBaseModel.py
@@ -106,10 +106,11 @@ class LightningModel(pl.LightningModule):
                 else self.batch_size
             )
             self.console_logger.info("global batch size is {}".format(global_batch_size))
-            train_iters_per_epoch = len(self.trainer.datamodule.train_data) / global_batch_size
+            train_iters_per_epoch = self.trainer.datamodule.len_train_data / global_batch_size
             self.console_logger.info(f"train iterations per epoch are {train_iters_per_epoch}")
             warmup_steps = int(train_iters_per_epoch * 10)
             total_steps = int(train_iters_per_epoch * self.trainer.max_epochs)
+            self.console_logger.info(f"Total train steps are {total_steps}")
 
             if self.cfg_scheduler == "linear_warmup_decay":
                 scheduler = {
@@ -297,12 +298,12 @@ class LightningModel(pl.LightningModule):
         return figure
 
     def validation_epoch_end(self, outputs):
-        if self.log_confusion_matrices:
+        if self.log_confusion_matrices and self.current_epoch > 0:
             self._log_accuracy_matrices("Validation")
             # we also log and reset the training CM, so we log a training CM everytime we log a validation CM
             self._log_accuracy_matrices("Training")
 
-        if self.log_tsne_image:
+        if self.log_tsne_image and self.current_epoch > 0:
             self.plot_tsne_images(outputs)
 
         if self.temperature_scale:
@@ -381,8 +382,8 @@ class LightningModel(pl.LightningModule):
         self.logger.experiment[0].add_figure(f"TSNE Scatter {name}", g.fig, self.global_step)
         plt.close("all")
 
-    def _correct_eval_probabilities_with_training_prior(self, outputs):
-        outputs = torch.exp(outputs)
+    def _correct_eval_probabilities_with_training_prior(self, log_probabilities):
+        outputs = torch.exp(log_probabilities)
         training_class_counts = torch.tensor(self.trainer.datamodule.training_class_counts).to(self.device)
         p_balanced_per_class = 1 / (len(training_class_counts))
         p_corrected_per_class = training_class_counts / len(training_class_counts)