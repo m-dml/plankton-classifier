1b89cfd51cd9c9511bd891fdcfcbd1ab6adac4dd
Tobias Machnitzki <tobias-machnitzki@web.de>
2022-05-04 13:20:53 +0200

    fixup new experiment

--- evaluate_singlelabel.py
+++ evaluate_singlelabel.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
             "/gpfs/work/machnitz/plankton_logs/supervised/singlelabel/multirun/2022-04-19/08-14-50"
         ],
         "finetune_singlelabel": [
-          "/gpfs/work/machnitz/plankton_logs/finetune/singlelabel/multirun/2022-04-19/08-15-24"
+            "/gpfs/work/machnitz/plankton_logs/finetune/singlelabel/multirun/2022-04-19/08-15-24"
         ],
         "linear_singlelabel": [
-          "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-04-29/08-31-47",
-          "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-05-02/10-33-55"
+            "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-05-04/11-40-25",
+            "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-05-04/11-46-50",
+            "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-05-04/12-17-30",
         ],
         "finetune_sgd_singlelabel": [
             "/gpfs/work/machnitz/plankton_logs/finetune_sgd/singlelabel/multirun/2022-04-29/08-19-35"
