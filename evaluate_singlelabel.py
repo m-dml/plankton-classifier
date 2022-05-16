79c620a80598e303e745b56953538ba9ff9e917a
Tobias Machnitzki <tobias-machnitzki@web.de>
2022-05-16 09:35:13 +0200

    new eval experiments

--- evaluate_singlelabel.py
+++ evaluate_singlelabel.py
@@ -4,18 +4,20 @@ from src.utils.eval_single_helper_funcs import get_best_checkpoints
 if __name__ == "__main__":
 
     experiments_paths = {
-        "fintune_frozen": ["/gpfs/work/machnitz/plankton_logs/finetune_frozen/singlelabel/multirun/2022-05-04/14-41-49"],
+        "fintune_frozen": [
+            "/gpfs/work/machnitz/plankton_logs/finetune_frozen/singlelabel/multirun/2022-05-06/14-46-05"
+        ],
         "supervised_singlelabel": [
-            "/gpfs/work/machnitz/plankton_logs/supervised/singlelabel/multirun/2022-05-04/14-35-37"
+            "/gpfs/work/machnitz/plankton_logs/supervised/singlelabel/multirun/2022-05-10/07-38-24"
         ],
         "finetune_singlelabel": [
-            "/gpfs/work/machnitz/plankton_logs/finetune/singlelabel/multirun/2022-04-19/08-15-24"
+            "/gpfs/work/machnitz/plankton_logs/finetune/singlelabel/multirun/2022-05-05/07-02-47"
         ],
         "linear_singlelabel": [
-            "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-05-04/14-40-43",
+            "/gpfs/work/machnitz/plankton_logs/linear_eval/singlelabel/multirun/2022-05-06/13-58-58",
         ],
         "finetune_sgd_singlelabel": [
-            "/gpfs/work/machnitz/plankton_logs/finetune_sgd/singlelabel/multirun/2022-05-04/19-41-16"
+            "/gpfs/work/machnitz/plankton_logs/finetune_sgd/singlelabel/multirun/2022-05-10/07-43-28"
         ],
     }
 
