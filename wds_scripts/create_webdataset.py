1eef01a53cf3a88afe12ad04bd3c5c41dd158132
Tobias Machnitzki <tobias.machnitzki@hzg.de>
2022-10-13 10:45:13 +0200

    Make webdataset creation a streaming operation
    Instead of first iterating over all files and
    afterwards processing them, this commit will
    change the code to process all files in one
    directory, as soon as that directory is found.
    Also, a verbose keyword is added.

--- wds_scripts/create_webdataset.py
+++ wds_scripts/create_webdataset.py
@@ -1,45 +1,50 @@
 import argparse
+import logging
 import os
 
 import webdataset as wds
 
 parser = argparse.ArgumentParser(description="Parameters for WebDataset creation", allow_abbrev=True)
-parser.add_argument("srcpath", help="Path to parent directory containing image files/folders")
-parser.add_argument("dstpath", help="Path to destination directory of the WebDataset")
+parser.add_argument("src_path", help="Path to parent directory containing image files/folders")
+parser.add_argument("dst_path", help="Path to destination directory of the WebDataset")
 parser.add_argument("--dst_prefix", default="", help="Prefix for shard file names")
 parser.add_argument("--unsupervised", help="Will the Dataset be used for Pretraining? (Bool)", action="store_true")
 parser.add_argument("--shard_size", default=1e9, help="Maximum size of Dataset Shard in Bytes")
+parser.add_argument("--verbose", action="store_true", help="Prints additional information")
 
 
-def get_list_of_files(dir_name):
-    list_of_files = list()
-    for (dirpath, dirnames, filenames) in os.walk(dir_name):
-        list_of_files += [os.path.join(dirpath, file) for file in filenames]
-    return list_of_files
+def create_unsupervised_dataset_from_folder_structure(
+    src_path, dst_path, dst_prefix, unsupervised, shard_size, *_, **__
+):
+    if not os.path.isdir(os.path.expanduser(dst_path)):
+        os.makedirs(os.path.expanduser(dst_path))
+        logging.info("Created Directory: {}".format(dst_path))
 
+    sink = wds.ShardWriter(os.path.join(dst_path, f".{dst_prefix}_data_shard-%07d.tar"), maxsize=shard_size)
+    for (dir_path, _, filenames) in os.walk(src_path):
+        list_of_files = [os.path.join(dir_path, file) for file in filenames]
+        logging.info("Processing folder: {}".format(dir_path))
 
-def create_unsupervised_dataset_from_folder_structure(srcpath, dstpath, dst_prefix, unsupervised, shard_size):
-    basenames = get_list_of_files(os.path.expanduser(srcpath))
-    if not os.path.isdir(os.path.expanduser(dstpath)):
-        os.makedirs(os.path.expanduser(dstpath))
-    sink = wds.ShardWriter(os.path.join(dstpath, dst_prefix + "data_shard-%07d.tar"), maxsize=shard_size)
-    for basename in basenames:
-        with open(basename, "rb") as stream:
-            image = stream.read()
-        clss = os.path.basename(os.path.dirname(basename))
-        if unsupervised:
-            sample = {
-                "__key__": os.path.splitext(basename)[0],
-                "input.png": image,
-            }
-        else:
-            sample = {"__key__": os.path.splitext(basename)[0], "input.png": image, "label.txt": clss}
+        for file in list_of_files:
+            with open(file, "rb") as stream:
+                image = stream.read()
+            clss = os.path.basename(os.path.dirname(file))
+            if unsupervised:
+                sample = {
+                    "__key__": os.path.splitext(file)[0],
+                    "input.png": image,
+                }
+            else:
+                sample = {"__key__": os.path.splitext(file)[0], "input.png": image, "label.txt": clss}
 
-        sink.write(sample)
+            sink.write(sample)
     sink.close()
     return True
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    logging.basicConfig(level=logging.WARNING)
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
     create_unsupervised_dataset_from_folder_structure(**vars(args))
