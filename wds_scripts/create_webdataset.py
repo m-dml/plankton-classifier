aadc8524257804d1d9b3e1cf11033fb790d0b7e1
Tobias Machnitzki <tobias.machnitzki@hzg.de>
2022-10-13 10:54:31 +0200

    Fixup: reimplement --extension keyword

--- wds_scripts/create_webdataset.py
+++ wds_scripts/create_webdataset.py
@@ -11,10 +11,11 @@ parser.add_argument("--dst_prefix", default="", help="Prefix for shard file name
 parser.add_argument("--unsupervised", help="Will the Dataset be used for Pretraining? (Bool)", action="store_true")
 parser.add_argument("--shard_size", default=1e9, help="Maximum size of Dataset Shard in Bytes")
 parser.add_argument("--verbose", action="store_true", help="Prints additional information")
+parser.add_argument("--extension", default="png", help="image format/ file extension (png/jpg)")
 
 
 def create_unsupervised_dataset_from_folder_structure(
-    src_path, dst_path, dst_prefix, unsupervised, shard_size, *_, **__
+    src_path, dst_path, dst_prefix, unsupervised, extension, shard_size, *_, **__
 ):
     if not os.path.isdir(os.path.expanduser(dst_path)):
         os.makedirs(os.path.expanduser(dst_path))
@@ -25,6 +26,11 @@ def create_unsupervised_dataset_from_folder_structure(
         list_of_files = [os.path.join(dir_path, file) for file in filenames]
         logging.info("Processing folder: {}".format(dir_path))
 
+        list_of_files = [
+            basename
+            for basename in list_of_files
+            if os.path.splitext(basename)[1].lower().strip() == "." + extension.lower().strip()
+        ]
         for file in list_of_files:
             with open(file, "rb") as stream:
                 image = stream.read()
