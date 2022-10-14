8fb6ba632f49e9ab1715f6e36199d8c579c1e011
Tobias <tobias.machnitzki@hzg.de>
2022-10-14 18:32:02 +0200

    Make webdataset creator faster (#42)
    * Add terminal tool for WebDataset creation
    
    * pre-commit change
    
    * Make get_list_of_files faster
    
    * Make webdataset creation a streaming operation
    
    Instead of first iterating over all files and
    afterwards processing them, this commit will
    change the code to process all files in one
    directory, as soon as that directory is found.
    Also, a verbose keyword is added.
    
    * Fixup: reimplement --extension keyword
    
    * First attempt of async webdataset creation
    
    * Fixup: wrong path inference
    
    * Add better logging
    
    * Add logging to sys out
    
    * More debug logging
    
    * Fixup: change dst_prefix -> dst_path
    
    * Trying to increase throughput
    
    * Playing around with asyncio run
    
    * trying concurrancy
    
    * trying to fix functools.partial
    
    * trying to work out asyncio
    
    * trying to work out asyncio
    
    * trying to work out asyncio
    
    * Trying new approach by using system calls
    
    * Trying new approach by using system calls
    
    * Trying new approach by using system calls
    
    * Trying new approach by using system calls
    
    * Add debug print
    
    * Fixup
    
    * Fixup
    
    * Fixup
    
    * Fixup shardsize
    
    Co-authored-by: smalldatalooser <kilian.huss@studium.uni-hamburg.de>
    Co-authored-by: Tobias Schanz <t.schanz@outlook.de>
    Co-authored-by: smalldatalooser <74614703+smalldatalooser@users.noreply.github.com>

--- wds_scripts/create_webdataset.py
+++ wds_scripts/create_webdataset.py
@@ -1,70 +1,72 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 12 14:06:25 2022
-
-@author: smalldatalooser
-"""
-
 import argparse
+import logging
 import os
+import sys
 
 import webdataset as wds
 
 parser = argparse.ArgumentParser(description="Parameters for WebDataset creation", allow_abbrev=True)
-parser.add_argument("srcpath", help="Path to parent directory containing image files/folders")
-parser.add_argument("dstpath", help="Path to destination directory of the WebDataset")
+parser.add_argument("src_path", help="Path to parent directory containing image files/folders")
+parser.add_argument("dst_path", help="Path to destination directory of the WebDataset")
 parser.add_argument("--dst_prefix", default="", help="Prefix for shard file names")
 parser.add_argument("--unsupervised", help="Will the Dataset be used for Pretraining? (Bool)", action="store_true")
-parser.add_argument("--shard_size", default=1e9, help="Maximum size of Dataset Shard in Bytes")
+parser.add_argument("--shard_size", default=1e9, help="Maximum size of Dataset Shard in Bytes", type=float)
+parser.add_argument("--verbose", action="store_true", help="Prints additional information")
 parser.add_argument("--extension", default="png", help="image format/ file extension (png/jpg)")
 
 
-def getListOfFiles(dirName):
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-
-    return allFiles
-
-
-def get_basenames(parent_dir):
-    return getListOfFiles(parent_dir)
-
-
 def create_unsupervised_dataset_from_folder_structure(
-    srcpath, dstpath, dst_prefix, unsupervised, shard_size, extension
+    src_path, dst_path, dst_prefix, unsupervised, extension, shard_size, *_, **__
 ):
-    basenames = getListOfFiles(os.path.expanduser(srcpath))
-    basenames = [bsnam for bsnam in basenames if os.path.splitext(bsnam)[1] == '.' + extension]
-    if not os.path.isdir(os.path.expanduser(dstpath)):
-        os.makedirs(os.path.expanduser(dstpath))
-    sink = wds.ShardWriter(os.path.join(dstpath, dst_prefix + "data_shard-%07d.tar"), maxsize=shard_size)
-    for basename in basenames:
-        with open(basename, "rb") as stream:
-            image = stream.read()
-        clss = os.path.basename(os.path.dirname(basename))
-        if unsupervised:
-            sample = {
-                "__key__": os.path.splitext(basename)[0],
-                "input.png": image,
-            }
-        else:
-            sample = {"__key__": os.path.splitext(basename)[0], "input.png": image, "label.txt": clss}
+    if not os.path.isdir(os.path.expanduser(dst_path)):
+        os.makedirs(os.path.expanduser(dst_path))
+        logging.info("Created Directory: {}".format(dst_path))
 
-        sink.write(sample)
+    sink = wds.ShardWriter(os.path.join(dst_path, f".{dst_prefix}_data_shard-%07d.tar"), maxsize=shard_size)
+    for (dir_path, _, filenames) in os.walk(src_path):
+        list_of_files = [os.path.join(dir_path, file) for file in filenames]
+        logging.info("Processing folder: {}".format(dir_path))
+
+        list_of_files = [
+            basename
+            for basename in list_of_files
+            if os.path.splitext(basename)[1].lower().strip() == "." + extension.lower().strip()
+        ]
+        for file in list_of_files:
+            with open(file, "rb") as stream:
+                image = stream.read()
+            clss = os.path.basename(os.path.dirname(file))
+            if unsupervised:
+                sample = {
+                    "__key__": os.path.splitext(file)[0],
+                    "input.png": image,
+                }
+            else:
+                sample = {"__key__": os.path.splitext(file)[0], "input.png": image, "label.txt": clss}
+
+            sink.write(sample)
     sink.close()
-    return True
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.StreamHandler(sys.stdout),
+                logging.FileHandler("create_webdataset.log")
+            ]
+        )
+    else:
+        logging.basicConfig(
+            level=logging.WARNING,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+
     create_unsupervised_dataset_from_folder_structure(**vars(args))
