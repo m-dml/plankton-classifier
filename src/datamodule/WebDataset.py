7bd29b0e71a456090007aa9cc4fdc83184f2e3a1
Tobias Schanz <t.schanz@outlook.de>
2022-11-03 09:12:59 +0100

    WIP: more work on the webdataloader

--- src/datamodule/WebDataset.py
+++ src/datamodule/WebDataset.py
@@ -1,52 +1,14 @@
-from abc import abstractmethod
+import os
 
 import hydra.utils
 import pytorch_lightning as pl
 import torch
 import webdataset
+import torchvision
+from src.utils import utils
 
 
-class BaseWebDataset(webdataset.WebDataset):
-    def __init__(self, integer_labels, transform, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.integer_labels = integer_labels
-        self.transform = transform
-
-
-class FinetuneWebDataset(BaseWebDataset):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __getitem__(self, item):
-        image, label_name = item["input"], item["label"]
-
-        if self.transform:
-            image = self.transform(image)
-
-        if not isinstance(image, torch.Tensor):
-            image = transforms.ToTensor()(image)
-
-        return image, (torch.Tensor([self.integer_labels[label_name]]), label_name)
-
-
-class PretrainWebDataset(BaseWebDataset):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __getitem__(self, item):
-        image = item["input"]
-        image_copy = image.copy()
-
-        if self.transform:
-            image = self.transform(image)
-            image_copy = self.transform(image_copy)
-        else:
-            raise ValueError("Transforms must be defined for pretraining")
-
-        return (image, image_copy), (torch.tensor([0]))
-
-
-class WebDataLoader(pl.LightningDataModule):
+class WebDataLoader(pl.LightningDataModule):  # noqa: D101
     def __init__(
         self,
         excluded_labels,
@@ -56,24 +18,30 @@ class WebDataLoader(pl.LightningDataModule):
         validation_split,  # The fraction size of the validation data (rest ist test)
         shuffle_train_dataset,  # whether to shuffle the train dataset (bool)
         shuffle_validation_dataset,
-        super_classes,
-        oversample_data,
+        super_classes,  # TODO: implement super classes
+        oversample_data,  # TODO: implement oversampling
         random_seed,
         train_transforms,
         valid_transforms,
         data_base_path,
         dataset,
         is_in_simclr_mode,
-        reduce_data,
-        pin_memory=False,
-        unlabeled_files_to_append=None,
-        is_ddp=False,
-        subsample_supervised=100,
+        subsample_supervised=100,  # TODO: implement subsampling
         shuffle_size=5000,
         *args,
         **kwargs
     ):
+        torch.manual_seed(random_seed)
         super().__init__(*args, **kwargs)
+
+        self.console_logger = utils.get_logger(__name__)
+        self.no_augmentations_log_state = False  # Used to log only once if no augmentations are defined
+        self.urls = []  # container for all .tar files
+
+        self.train_data = None
+        self.validation_data = None
+        self.test_data = None
+
         self.excluded_labels = excluded_labels
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -84,31 +52,82 @@ class WebDataLoader(pl.LightningDataModule):
         self.data_base_path = data_base_path
         self.train_transforms = train_transforms
         self.valid_transforms = valid_transforms
+        self.is_in_simclr_mode = is_in_simclr_mode
+        self.train_split = train_split
+        self.validation_split = validation_split
 
-    def make_loader(self, paths, mode="fit"):
+    def prepare_data(self, *args, **kwargs):
+        for dir_path, _, filenames in os.walk(self.data_base_path):
+            for file in filenames:
+                if file.endswith(".tar"):
+                    self.urls.append(os.path.join(dir_path, file))
+
+        self.train_data = self.urls[: int(len(self.urls) * self.train_split)]
+        self.validation_data = self.urls[int(len(self.urls) * self.train_split): int(len(self.urls) * self.validation_split)]
+        self.test_data = self.urls[int(len(self.urls) * self.validation_split):]
+
+    def make_loader(self, urls, mode="fit"):
         shuffle = 0
         if mode == "fit":
-            transforms = hydra.utils.instantiate(self.train_transforms)
+            augmentations: torchvision.transforms = hydra.utils.instantiate(self.train_transforms)
             if isinstance(self.shuffle_train_dataset, bool):
                 shuffle = self.shuffle_size if self.shuffle_train_dataset else 0
 
         elif mode == "eval":
-            transforms = hydra.utils.instantiate(self.valid_transforms)
+            augmentations: torchvision.transforms = hydra.utils.instantiate(self.valid_transforms)
             if isinstance(self.shuffle_validation_dataset, bool):
                 shuffle = self.shuffle_size if self.shuffle_validation_dataset else 0
         else:
             raise ValueError("Mode must be either 'fit' or 'eval'")
 
-        dataset = (
-            hydra.utils.instantiate(self.cfg_dataset)
-            .shuffle(shuffle)
-            .decode("pil")
-            .tu_tuple()
-            .map_tuple(transforms, lambda x: x)  # lamda for not transforming the labels (identity func)
-            .batched(self.batch_size, partial=False)
-        )
+        if self.is_in_simclr_mode:
+            dataset = (
+                webdataset.WebDataset(urls)
+                .shuffle(shuffle)
+                .decode("pil")
+                .map(lambda x: self.transform(x, augmentations))
+                .batched(self.batch_size, partial=False)
+            )
 
-        loader = webdataset.WebLoader(dataset, batch_size=None, shuffle=False, num_workers=self.num_workers)
+        else:
+            dataset = (
+                webdataset.WebDataset(urls)
+                .shuffle(shuffle)
+                .decode("pil")
+                .tu_tuple()
+                .map_tuple(lambda x: self.transform(x, augmentations), lambda x: x)
+                .batched(self.batch_size, partial=False)
+            )
 
-        if mode is "fit":
-            loader = loader.ddp_equalize(dataset_size // self.batch_size)
+        return webdataset.WebLoader(dataset, batch_size=None, shuffle=False, num_workers=self.num_workers)
+
+    def transform(self, image, augmentations):
+        if self.is_in_simclr_mode:
+            image_copy = image.copy()
+            if augmentations:
+                image = augmentations(image)
+                image_copy = augmentations(image_copy)
+
+                if image.equals(image_copy):
+                    self.console_logger.warning("Image and image copy are equal")
+            else:
+                raise ValueError("Transforms must be defined for pretraining")
+
+            return image, image_copy
+
+        else:
+            if augmentations:
+                image = augmentations(image)
+            elif not self.no_augmentations_log_state:
+                self.no_augmentations_log_state = True
+                self.console_logger.warning("No augmentations defined. Using original image")
+            return image
+
+    def train_dataloader(self):
+        return self.make_loader(self.train_data, mode="fit")
+
+    def val_dataloader(self):
+        return self.make_loader(self.validation_data, mode="eval")
+
+    def test_dataloader(self):
+        return self.make_loader(self.test_data, mode="eval")
